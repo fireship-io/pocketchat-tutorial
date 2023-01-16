6262536eaca2e015cef334d804b9daefebc21111
pawitsahare0 <119015232+pawitsahare0@users.noreply.github.com>
2023-01-17 02:56:35 +0530

    Update pocketbase.ts

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('https://pawit-prod-pawitsa-y4sfjt.mo6.mogenius.io:80'); // remote
+export const pb = new PocketBase('https://pawit-prod-pawitsa-y4sfjt.mo6.mogenius.io'); // remote
 // const pb = new PocketBase('http://127.0.0.1:8090'); // local
 
 export const currentUser = writable(pb.authStore.model);
